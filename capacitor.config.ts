0f126f44ca81ee8178d354b5b3966fabc7d3f008
kosmonox <70446391+kosmonox@users.noreply.github.com>
2026-08-02 22:49:58 +0200

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.apneatrainer.app',
+  appName: 'Apnea Trainer',
+  webDir: 'www'
+};
+
+export default config;
